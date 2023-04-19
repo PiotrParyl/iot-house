851f7423b01e5aef18236890c43ddd28ded308f5
PiotrParyl <piotrekzrydek@gmail.com>
2023-04-19 23:19:07 +0200

    add hp to db

---
+++ test_db.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+
+
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    user="maczo420all",
+    passwd="Pomidor13",
+    database="metryka",
+)
+
+mycursor = db.cursor()
+
+mycursor.execute('SELECT * FROM woda ORDER BY id DESC, datechat DESC LIMIT 2')
+
+for ziom in mycursor:
+    print(ziom)
